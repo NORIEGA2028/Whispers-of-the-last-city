874a3e88e30cf9c3e70565e78c336c1a8cddbd04
NORIEGA2028 <kaonoriega@gmail.com>
2025-11-03 01:18:39 +0800

    Reapply "SFX"
    This reverts commit 5afa3112d0dca478f883fad44d88f1e9752ab05d.

--- Assets/Scripts/NPCInteractions.cs
+++ Assets/Scripts/NPCInteractions.cs
@@ -2,34 +2,38 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class NPCSystem : MonoBehaviour
+public class NPCInteractions : MonoBehaviour
 {
     bool player_detection = false;
 
-    // Update is called once per frame
     void Update()
     {
-        
-        if(player_detection && Input.GetKeyDown(KeyCode.E))
+        // Support keyboard for PC testing
+        if (player_detection && Input.GetKeyDown(KeyCode.E))
         {
-            print("PLEASE WORK!");
+            Interact();
         }
-
     }
 
     private void OnTriggerEnter(Collider other)
     {
-        if(other.name == "Player")
+        if (other.CompareTag("Player"))
         {
             player_detection = true;
         }
     }
-    
 
     private void OnTriggerExit(Collider other)
     {
         player_detection = false;
     }
 
-}
+    public void Interact()
+    {
+        if (player_detection)
+        {
+            Debug.Log("Interaction successful!");
 
+        }
+    }
+}
